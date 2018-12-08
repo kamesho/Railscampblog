a16bae09e0d75ac9d2add2e23307ed57f1d8f62e
EC2 Default User <ec2-user@ip-172-31-12-195.ap-southeast-1.compute.internal>
2018-12-08 20:10:49 +0000

    Finish StaticPages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root 'staticpages#home'
   get 'staticpages/home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ 
   
-  root 'application#index'
 end
