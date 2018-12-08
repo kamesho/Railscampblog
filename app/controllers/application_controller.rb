e0fbbc8843e21dcd5b31f265ae53d40aed9d70f5
EC2 Default User <ec2-user@ip-172-31-12-195.ap-southeast-1.compute.internal>
2018-12-08 23:15:10 +0000

    ログイン機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
   
-  def index
-    render html: "hello,world!"
-  end
+   
 end
---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,6 @@
+class PagesController < ApplicationController
+  before_action :sign_in_required, only: [:show]
+
+  def show
+  end
+end
